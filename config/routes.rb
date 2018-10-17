4b4bbfd1798959b2cfe04cd88ad084c8b3997ad7
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-10-17 15:14:07 -0700

    created a routes for product

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get 'user/destroy'
   get 'user/edit'
   get 'user/create'
+
+  resources :products
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
