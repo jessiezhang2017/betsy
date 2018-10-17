da4f834a5b64f818fb53b276f9930e2eac279ef1
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-17 15:11:29 -0700

    created columns for cc info to users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
+  #before_action is type merchant?
+
   def index
     @users = User.all
   end
@@ -8,23 +10,27 @@ class UserController < ApplicationController
   end
 
   def create
+    #check if user wants merchant status
   end
 
   def show
   end
 
   def update
-  end
-
-  def destroy
+    #can become merchant?
   end
 
   def edit
+    #can become merchant?
+  end
+
+  def destroy
+    #delete account aka be sent to siberia
   end
 
   private
 
   def user_params
-    return params.require(:user).permit(:name, :address, :email, :cc_num, :merchant_id)
+    return params.require(:user).permit(:name, :address, :email, :cc_num, :cc_csv, :cc_exp, :type, :bill_zip)
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,3 @@
 class Merchant < User
+  #merchant inherits all things from user, but when a new merchant is created the 'type' is automagically set to merchant.
 end
