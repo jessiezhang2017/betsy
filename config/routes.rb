3f977692ea58a110c8f23426a28227f5db116229
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-18 13:49:33 -0700

    merged conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,4 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, except: [:destroy]
 
-<<<<<<< HEAD
-
-=======
->>>>>>> fa260d3c7645cae5a1476e7e60804b62b10d1ba2
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
