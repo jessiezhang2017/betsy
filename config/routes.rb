6e7cd9e88d143fb969b78078475bf636b6a95973
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-18 10:53:15 -0700

    working on omniauth sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  get "/auth/:provider/callback", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy", as: "logout"
+  get "/auth/:provider/callback", to: "users#create"
+  # delete "/logout", to: "sessions#destroy", as: "logout"
 
   resources :users
   resources :orders, except: [:destroy]
 
-  get 'user/index'
-  get 'user/new'
-  get 'user/show'
-  get 'user/update'
-  get 'user/destroy'
-  get 'user/edit'
-  get 'user/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
