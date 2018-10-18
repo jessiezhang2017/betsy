a01b3ac945a20e927e5af9fb668cfbad799390fd
Jacquelyn Cheng <35181875+jacquelynoelle@users.noreply.github.com>
2018-10-18 10:54:05 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,5 @@ Rails.application.routes.draw do
   resources :users
   resources :orders, except: [:destroy]
 
-  get 'user/index'
-  get 'user/new'
-  get 'user/show'
-  get 'user/update'
-  get 'user/destroy'
-  get 'user/edit'
-  get 'user/create'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
