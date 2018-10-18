b561d0c85d6ef264c55ac69ce49e034e5d0bf8df
jackie <jacquelyn.cheng@me.com>
2018-10-18 15:49:32 -0700

    Remove merge conflict markers from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,7 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, except: [:destroy]
 
-<<<<<<< HEAD
-=======
-
   resources :category, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 15e082d8069160141aa6a3cb60f5180ca973f182
 end
