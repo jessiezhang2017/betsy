374049653c90784e75b7d653974a424d23d75210
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-19 07:11:54 -0700

    nav added

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,6 @@ Rails.application.routes.draw do
   resources :users
   resources :products
   resources :orders, except: [:destroy]
-
-<<<<<<< HEAD
-=======
-
   resources :category, only: [:new, :create]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 15e082d8069160141aa6a3cb60f5180ca973f182
 end
