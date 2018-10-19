76959c351a2bc050db4cd380814694509d84fff6
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-19 13:49:34 -0700

    user controller test skeletons added

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -2,6 +2,22 @@ require "test_helper"
 
 describe UserController do
 
+  describe Merchant do
+
+    it "should allow edits to own products" do
+
+    end
+
+    it "should not allow type-merchant to purchase own products" do
+
+    end
+
+    it "allows a merchant to downgrade to a regular user" do
+
+    end 
+
+  end #merchant tests end
+
   it "logs in an existing user" do
     start_count = User.count
 
@@ -12,6 +28,35 @@ describe UserController do
     # Should *not* have created a new user
     User.count.must_equal start_count
   end
+
+  it "does not allow edits to products" do
+
+  end
+
+  it "does not allow deletion of products" do
+
+  end
+
+  it "allows user to edit quantity in order(shopping cart)" do
+
+  end
+
+  it "allows user to remove items to cart" do
+
+  end
+
+  it "allows user to add items to cart" do
+
+  end
+
+  it "allows user to edit their user information" do
+
+  end
+
+  it "allows a user to become a merchant" do
+
+  end
+
 
   it "should get index" do
     get user_index_url
