fd74d67391482d1ab8814647ae750c6233a94390
jackie <jacquelyn.cheng@me.com>
2018-10-19 13:51:56 -0700

    Fixed issue with order validation

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -1,13 +1,16 @@
-require 'pry'
-
 class OrderProductsController < ApplicationController
 
   def create
+    # pull in product and quantity from form partial on product show page
     product = Product.find_by(id: params[:order_product][:product_id].to_i)
     quantity = params[:order_product][:quantity].to_i
 
+    # create the orderproduct, add it to the current cart, and save it
     @current_order.add_product(product, quantity)
-    # check quantity against stock?
+
+    # save the cart and update the session
+    @current_order.save
+    session[:order_id] = @current_order.id
 
     redirect_to cart_path
   end
