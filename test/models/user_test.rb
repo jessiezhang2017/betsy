77f7e0bfb900d3a2ea2bf003e78c3522da51e975
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-19 14:19:34 -0700

    merged

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,14 @@
 require "test_helper"
 
 describe User do
+
+  describe Merchant do
+
+  end
+  
   let(:user) { :user1 }
 
   it "must be valid" do
-    value(user).must_be :valid?
+    value(:user).must_be :valid?
   end
 end
