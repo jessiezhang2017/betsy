716419c06ba698c8f4324e61648730f6d49f179a
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-10-20 19:29:45 -0700

    updated prod_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,8 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-
+    # @products_by_category = Product.to_category_hash.in_stock
+    # @pruducts_by_merchant = Product.to_merchant_hash.in_stock
 
   end
 
@@ -14,26 +15,54 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    if params[:user_id]
+      @user_id = params[:user_id].to_i
+      user = User.find_by(id: @user_id)
+      if user.nil?
+        flash.now[:warning] = "That user doesn't exit"
+      end
+      @product.user_id = @user_id
 
+    end
   end
 
 
   def create
+      @user_id = session[:uer_id].to_i
+      merchant = Merchant.find_by(id: @user_id)
 
+      if merchant.nil?
+        flash.now[:danger] = 'Not a merchant!'
+        redirect_to products_path
+      else
+        @product = Product.new(product_params)
+        product.user = merchant
+        if @product.save
+           flash[:success] = 'Product Created!'
+           redirect_to products_path
+        else
+           flash.now[:danger] = 'Product not created!'
+           render :new, status: :bad_request
+        end
+      end
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
-
+    if @product && @product.update(product_params)
+      redirect_to product_path(@product.id)
+    elsif @product
+      render :edit, status: :bad_request
+    end
   end
 
   def destroy
-
-
-
+    unless @product.nil?
+      @product.destroy
+      flash[:success] = "#{@product.name} deleted"
+      redirect_to root_path
+    end
   end
 
   private
