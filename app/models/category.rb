a99262d65569ee371788f30828120996dc6a224c
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-10-22 13:05:32 -0700

    added category list method to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ApplicationRecord
   has_many :products
   validates :name, presence: true
+
+  def self.category_list
+   return Category.all.map do |category|
+    [category.name, category.id]
+   end
+ end
 end
