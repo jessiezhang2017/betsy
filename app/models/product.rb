0926031163320d2ea493c31004bdf245da390b52
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-10-22 09:42:03 -0700

    resolve merge conflict

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,8 @@ class Product < ApplicationRecord
   validates :user_id, presence: true
   validates :stock, presence: true, numericality: { :only_integer => true, :greater_than_or_equal_to => 0}
 
-
+  validates :price, presence: true, numericality: {:greater_than_or_equal_to => 0}
+  validates :category_id,  presence: true
 
  def self.active_products
    return Product.all.select {|e| e.status == true}
