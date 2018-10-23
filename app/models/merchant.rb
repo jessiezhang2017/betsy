2738ee6b4f30910ce4714e97fcbe7efd7936dba8
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-23 13:32:41 -0700

    db migrated

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,9 @@
 class Merchant < User
+  has_many :products
   validates :type, presence: true
   #merchant inherits all things from user, but when a new merchant is created the 'type' is automagically set to merchant.
 
   #create method to be sure it is not nil, then add to validation
   #merchant needs ability to fulfill orders as 'shipped'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :products
+  has_many :orders
 
 
     def self.build_from_github(auth_hash)
