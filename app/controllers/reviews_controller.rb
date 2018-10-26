b07aef57d7c7291ca0cb6031277c9d58c4cb314c
Jacquelyn Cheng <35181875+jacquelynoelle@users.noreply.github.com>
2018-10-25 22:10:51 -0700

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,7 @@ class ReviewsController < ApplicationController
   def create
     product = Product.find_by(params[:id])
     if @current_user.is_a_merchant?
-      if @current_user .id != product.user_id
+      if @current_user.id != product.user_id
          review = product.reviews.new(review_params)
       end
 
